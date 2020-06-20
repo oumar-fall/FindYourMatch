ae5de49e98c1fceae70c42d3435400993f45bfe1
Fall <oumar.fall@telecom-paristech.fr>
2020-06-20 17:34:01 +0200

    fin modal sans graphique

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ const w = parseFloat(getComputedStyle(canvas).width);
 const modal = document.getElementById("modal");
 let x, y, zoomState;
 let dataset = [];
+const races = [
+    "Black/African American",
+	"European/Caucasian-American",
+	"Latino/Hispanic American",
+	"Asian/Pacific Islander/Asian-American",
+	"Native American",
+	"Other"
+]
 
 let svg = d3.select("#canvas")
             .append("svg")
@@ -54,7 +62,7 @@ function draw() {
                 .attr("cy", (d) => y(d.income))
                 .attr("fill", "red")
                 .attr("class", "data-entry")
-                .on("click", showModal);
+                .on("click", (d) => showModal(d));
 
     zoomed(g)
     zoom = d3.zoom()
@@ -114,7 +122,7 @@ function drawCluster(){
               .attr("cy", (d) => h/3*(1+(d.wave % 2)) + compute_cluster_y(2*Math.PI*d.idg/(d.round*2), (d.age-10)*3))
               .attr("fill", (d) => color(d.gender))
               .attr("class", "data-entry")
-              .on("click", showModal);
+              .on("click", (d) => showModal(d));
 }
 
 function onMouseOver(d, i){
@@ -147,6 +155,87 @@ function Cluster(){
   drawCluster();
 }
 
-function showModal(d, i){
+function getGender(id) {
+    return dataset.find(function(x) {
+        return x.iid == id;
+    }).gender;
+
+}
+
+function showModal(d){
+    var id = d.iid;
     modal.style.display = "flex";
+
+    addModalInfos(d);
+
+    var meetings = dataset.filter(function(x) {return x.iid == id});
+    for (var i in meetings){
+        var item = {};
+        item.gender = (getGender(meetings[i].pid)==1)? "man" : "woman";
+        item.id = meetings[i].pid;
+        if (meetings[i].match == 1) {
+            addModalMatchItem(item);
+        }
+        else {
+            addModalNoMatchItem(item);
+        }
+    }
+}
+function closeModal() {
+    modal.innerHTML = '<div class="modal-container"><div id="modal-close" onclick="closeModal()">X</div><div class="modal-content" id="modal-infos"><span class="modal-title" id="modal-infos-id"></span><div class="modal-content" id="modal-infos-details"><img id="modal-infos-img" src="media/man.svg" alt=""><div class="modal-content modal-content-2" id="modal-infos-details-text"><span class="modal-infos-details-span" id="modal-infos-age"></span><span class="modal-infos-details-span" id="modal-infos-from"></span><span class="modal-infos-details-span" id="modal-infos-race"></span><span class="modal-infos-details-span" id="modal-infos-field"></span><span class="modal-infos-details-span" id="modal-infos-income"></span><span class="modal-infos-details-span" id="modal-infos-undergraduate"></span></div></div></div><div class="modal-content" id="modal-matchs"><div class="modal-content modal-content-2" id="modal-match"><span class="modal-title" id="modal-match-title">Match</span> </div><div class="modal-content modal-content-2" id="modal-nomatch"><span class="modal-title" id="modal-nomatch-title">No Match</span></div></div><div class="modal-content" id="modal-graphic"></div></div>'
+    modal.style.display = "none";
+}
+
+function addModalMatchItem(item) {
+    var itemDiv = document.createElement('div');
+    itemDiv.classList.add("modal-match-item");
+    itemDiv.onclick = () => {
+        closeModal();
+        showModal(item.id);
+    }
+
+    var itemImg = document.createElement('img');
+    itemImg.src = "media/" + item.gender + ".svg";
+    itemImg.alt = item.gender;
+    itemImg.classList.add("modal-match-item-img");
+
+    var itemSpan = document.createElement('span');
+    itemSpan.classList.add("modal-match-item-span");
+    itemSpan.innerText = item.id;
+
+    itemDiv.appendChild(itemImg);
+    itemDiv.appendChild(itemSpan);
+    document.getElementById("modal-match").appendChild(itemDiv);
+}
+function addModalNoMatchItem(item) {
+    var itemDiv = document.createElement('div');
+    itemDiv.classList.add("modal-match-item");
+    itemDiv.onclick = () => {
+        closeModal();
+        showModal(item.id);
+    }
+
+    var itemImg = document.createElement('img');
+    itemImg.src = "media/" + item.gender + ".svg";
+    itemImg.alt = item.gender;
+    itemImg.classList.add("modal-match-item-img");
+
+    var itemSpan = document.createElement('span');
+    itemSpan.classList.add("modal-match-item-span");
+    itemSpan.innerText = item.id;
+
+    itemDiv.appendChild(itemImg);
+    itemDiv.appendChild(itemSpan);
+    document.getElementById("modal-nomatch").appendChild(itemDiv);
+}
+
+function addModalInfos(d) {
+    document.getElementById("modal-infos-id").innerHTML = "<strong>ID : </strong><em>" + d.iid + "</em>";
+    document.getElementById("modal-infos-age").innerHTML = "<strong>Age : </strong><em>" + d.age + "</em>";
+    document.getElementById("modal-infos-from").innerHTML = "<strong>From : </strong><em>" + d.from + "</em>";
+    document.getElementById("modal-infos-race").innerHTML = "<strong>Race : </strong><em>" + races[d.race-1] + "</em>";
+    document.getElementById("modal-infos-field").innerHTML = "<strong>Field of studies : </strong><em>" + d.field + "</em>";
+    document.getElementById("modal-infos-undergraduate").innerHTML = "<strong>Undergraduate School : </strong><em>" + d.undergra + "</em>";
+    document.getElementById("modal-infos-income").innerHTML = "<strong>Income : </strong><em>" + d.income + "</em>";
+    document.getElementById("modal-infos-img").src = "media/" + ((d.gender==1)? "man" : "woman") + ".svg";
 }
