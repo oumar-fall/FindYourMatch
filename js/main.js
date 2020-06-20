b1034f8192f0547e98cc4c735389d9bfdfc429e9
Marion Candelier <marion.candelier@telecom-paristech.fr>
2020-06-20 14:21:53 +0200

    filtre suivant l'age

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const canvas = document.getElementById('canvas');
 const h = parseFloat(getComputedStyle(canvas).height);
 const w = parseFloat(getComputedStyle(canvas).width);
 const modal = document.getElementById("modal");
+var agemax = 55;
+var agemin = 18;
 let x, y, zoomState;
 let dataset = [];
 
@@ -142,16 +144,45 @@ function drawFilter(){
     svg.selectAll("*").remove();
     var divtext  = document.getElementById("textdiv");
     var div = document.createElement(div);
-    div.innerHTML = "<p>Audio settings:</p> <div> <input type='range' list='tickmarks'id='age' name='age' min='0' max='11' oninput = 'voir1(age.value)'> <label for='volume'>Age</label> <br> <br> </div> <div><input type='range' id='cowbell' name='cowbell' list='tickmarks2' min='0' max='100' value='90' step='10' oninput = 'voir1(volume.value)'> <label for='cowbell'>Cowbell</label></div>";
+    div.innerHTML = "<p>Audio settings:</p> <div> <input type='range' list='tickmarks'id='age' name='age' min='18' max='55' oninput = 'ageMax(age.value)'> <label for='volume'>Age Max </label> <br> <br> </div> <div><input type='range' id='agem' name='agem' list='tickmarks2' min='18' max='55'  oninput = 'ageMin(agem.value)'> <label for='cowbell'>Age Min </label></div>";
     divtext.appendChild(div);
 }
 
-function voir1(nom){
-    console.log(nom);
+
+function visible(age){
+    if (age<agemax && age>agemin){
+        return("visible");
+    }
+    else {
+        return("hidden");
+    }
 }
 
-function voir2(nom){
+function afficherFilter(){
+    svg.selectAll("*").remove();
+    var g = svg.append('g');
+    g.selectAll("circle")
+                .data(dataset)
+                .enter().append("circle")
+                .attr("r", 2)
+                .attr("cx", (d) => w/23*d.wave + compute_cluster_x(2*Math.PI*d.idg/(d.round*2), (d.age-10)*3))
+                .attr("cy", (d) => h/3*(1+(d.wave % 2)) + compute_cluster_y(2*Math.PI*d.idg/(d.round*2), (d.age-10)*3))
+                .attr("fill", (d) => color(d.gender))
+                .attr("visibility",(d) => visible(d.age))
+                .attr("class", "data-entry")
+                .on("click", showModal);
+}
+function ageMax(nom){
+    agemax = parseInt(nom);
     console.log(nom);
+    afficherFilter();
+    
+  }
+
+function ageMin(nom){
+    agemin = parseInt(nom);
+    console.log(agemin);
+    afficherFilter();
 }
 
 function noCluster(){
