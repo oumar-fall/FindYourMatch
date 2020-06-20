96d4a5836f308db05cffb5d8339c28b2bd295123
Julie <julie.keisler@telecom-paristech.fr>
2020-06-20 17:35:39 +0200

    links between people

--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ let svg = d3.select("#canvas")
 
 var xAxis, yAxis, gX, gY;
 
-
 d3.csv("data/SpeedDating.csv")
     .row( (d, i) => {
         d.income = d.income.replace(',', '');
@@ -105,35 +104,80 @@ function zoomed(g){
 
 function drawCluster(){
   svg.selectAll("*").remove();
+
   var g = svg.append('g');
+  var relationships = createRelationships();
   g.selectAll("circle")
               .data(dataset)
               .enter().append("circle")
               .attr("r", 2)
-              .attr("cx", (d) => w/23*d.wave + compute_cluster_x(2*Math.PI*d.idg/(d.round*2), (d.age-10)*3))
-              .attr("cy", (d) => h/3*(1+(d.wave % 2)) + compute_cluster_y(2*Math.PI*d.idg/(d.round*2), (d.age-10)*3))
+              .attr("cx", (d) => compute_cluster_x(d))
+              .attr("cy", (d) => compute_cluster_y(d))
               .attr("fill", (d) => color(d.gender))
               .attr("class", "data-entry")
-              .on("click", showModal);
-}
+              .on("click", showModal)
+              .on("mouseover",function(d){textarea.innerHTML =d.age + ", " + d.field + ", " + d.income + " position " + compute_cluster_x(d) + " " + compute_cluster_y(d)});
+  var line = d3.line()
+  .x(function (d) { return d.x; })
+  .y(function (d) { return d.y; });
+
+  for (var i=0; i < relationships.length; i++) {
+    svg.append("path")
+      .attr("class", "plot")
+      .datum(relationships[i])
+      .attr("d", line);
+    }
+  }
 
 function onMouseOver(d, i){
 
 }
 
-function compute_cluster_y(angle, module){
-  return(module*Math.cos(angle));
+function match(d){
+  if (d.match == "1"){
+    return(d.pid);
+  }
 }
 
-function compute_cluster_x(angle, module){
-  return(module*Math.sin(angle));
+function createRelationships(){
+  console.log(dataset.length);
+  var r = [];
+  for (let i = 0; i < dataset.length; i++){
+    var d = dataset[i];
+    var id = match(d);
+    if (id){
+      var rel_i = i;
+      while ((rel_i < dataset.length-1) && (d.wave == dataset[rel_i].wave) && (id != dataset[rel_i].iid)){
+        rel_i = rel_i + 1;
+      }
+      if(id == dataset[rel_i].iid){
+        var rel = dataset[rel_i];
+        console.log(rel.wave + "/" + d.wave);
+        var relation = [{"x":compute_cluster_x(d), "y":compute_cluster_y(d)}, {"x":compute_cluster_x(rel), "y":compute_cluster_y(rel)}];
+        r.push(relation);
+      }
+    }
+  }
+  return(r);
+}
+
+function compute_cluster_y(d){
+  var angle = 2*Math.PI*d.idg/(d.round*2);
+  var mod = (d.age-10)*3;
+  return( h/3*(1+(d.wave % 2)) + mod*Math.sin(angle));
+}
+
+function compute_cluster_x(d){
+  var angle = 2*Math.PI*d.idg/(d.round*2);
+  var mod = (d.age-10)*3;
+  return(w/23*d.wave + mod*Math.cos(angle));
 }
 
 function color(int){
   switch(int){
-		case "1":
-			return("rgb(245,166,241)");
 		case "0":
+			return("rgb(245,166,241)");
+		case "1":
 			return("rgb(83,80,242)");
     }
 }
